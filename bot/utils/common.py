b1f8b089a5902a5cb332036fb13007cdfbf484d1
Bogdan <evstrat.bg@mail.ru>
2018-01-27 21:09:13 +0300

    fix start msg

--- bot/utils/common.py
+++ bot/utils/common.py
@@ -12,15 +12,22 @@ def start(bot: Bot, update: Update):
     first_name = update.message.from_user.first_name
     username = update.message.from_user.username
     last_name = update.message.from_user.last_name
-    Users.get_or_create(
-        telegram_id=uid,
-        first_name=first_name,
-        last_name=last_name,
-        username=username
-    )
+
+    user = Users.get_or_create(telegram_id=uid)
+    user.first_name = first_name,
+    user.last_name = last_name,
+    user.username = username
+    user.save()
+
     bot.send_message(
         uid,
-        'Привет! Чем тебе помочь?',
+        '''Привет. Я SAuto для поиска , ремонта и продажи вашего автомобиля. Я свяжу вас напрямую с личным менеджером, который будет закреплён за вами после принятии, оставленной вами заявки. Менеджер полностью проконсультирует вас по любому вопросу ремонта вашего авто , продаже и поможет подобрать машину вашей мечты. В чем плюсы?
+
+1. Машина будет соответствовать вашим деньгам, будет лучшим выбором на вторичном рынке как по цене , так и по общему состоянию. Найденный нами автомобиль будет не только лучшим, но и очень выгодным.
+
+2. Ремонт вашего Авто будет произведено в сервисах, которые проверенные тысячами автолюбителями и являются самыми близкими к вам так же выгодными. 
+
+3. Продажа вашего Авто не принесёт вам хлопот. Вы получите ту сумму, которую ждёте, без ваших усилили , мы сделаем все за вас.''',
         reply_markup=ReplyKeyboardMarkup(
             keyboard_start
         )
